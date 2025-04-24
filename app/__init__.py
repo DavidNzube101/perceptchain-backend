c225cfba8f887100a86b5dc6e80dcd7148f3799c
DavidNzube101 <davidnzube2007@outlook.com>
2025-04-24 19:14:49 +0100

    Add redirect from root to Swagger documentation

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, redirect
 from flask_cors import CORS
 from .config import Config
 from .routes.health import health_bp
@@ -18,5 +18,9 @@ def create_app(config_class=Config):
     @app.route('/docs')
     def docs():
         return send_from_directory(app.static_folder, 'swagger-ui.html')
+    
+    @app.route('/')
+    def default():
+        return redirect('/docs', code=302)
 
     return app
